3e13029c03eb9a148a56b075687cef9d4abae28e
iNikitaGricenko <VfVristalt@gmail.com>
2022-03-10 20:15:40 +0200

    refactor repositories (small)

--- src/main/java/com/wolfhack/diploma/repository/LaptopRepository.java
+++ src/main/java/com/wolfhack/diploma/repository/LaptopRepository.java
@@ -1,9 +1,11 @@
 package com.wolfhack.diploma.repository;
 
 import com.wolfhack.diploma.models.products.Laptop;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface LaptopRepository extends ProductRepo<Laptop> {
 
     List<Laptop> findLaptopsByCostLessThanEqual(double cost);
--- src/main/java/com/wolfhack/diploma/repository/SsdRepository.java
+++ src/main/java/com/wolfhack/diploma/repository/SsdRepository.java
@@ -1,6 +1,8 @@
 package com.wolfhack.diploma.repository;
 import com.wolfhack.diploma.models.products.Ssd;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SsdRepository extends ProductRepo<Ssd> {
 
 }
