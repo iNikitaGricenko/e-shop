e749ed1f4a42c3ee91cf1ca687e3d4f3963ce967
iNikitaGricenko <VfVristalt@gmail.com>
2022-06-07 12:11:48 +0300

    refactor code

--- src/main/java/com/wolfhack/diploma/repository/RamRepository.java
+++ src/main/java/com/wolfhack/diploma/repository/RamRepository.java
@@ -1,8 +1,9 @@
 package com.wolfhack.diploma.repository;
 
 import com.wolfhack.diploma.models.products.Ram;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RamRepository extends MongoRepository<Ram> {
+public interface RamRepository extends MongoRepository<Ram, String> {
 }
