e124e873b6fa0075e60ae70e82be2ed6069ad77a
iNikitaGricenko <VfVristalt@gmail.com>
2022-06-13 01:23:41 +0300

    add wishlist

--- src/main/java/com/wolfhack/diploma/repository/GpuRepository.java
+++ src/main/java/com/wolfhack/diploma/repository/GpuRepository.java
@@ -1,8 +1,13 @@
 package com.wolfhack.diploma.repository;
 
 import com.wolfhack.diploma.models.products.Gpu;
+import com.wolfhack.diploma.models.products.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface GpuRepository extends MongoRepository<Gpu, String> {
--- src/main/java/com/wolfhack/diploma/repository/ProductRepository.java
+++ src/main/java/com/wolfhack/diploma/repository/ProductRepository.java
@@ -1,21 +1,14 @@
 package com.wolfhack.diploma.repository;
 
 import com.wolfhack.diploma.models.products.Product;
-import com.wolfhack.diploma.models.products.Ram;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface RamRepository extends MongoRepository<Ram, String> {
-
-    boolean existsProductByNameAndModel(String name, String model);
-    Ram findByNameIsLikeAndModelIsLike(String name, String model);
-
-    Page<Ram> findAllByType(Pageable pageable, String s);
-
+public interface ProductRepository extends MongoRepository<Product, String> {
+    Page<Product> findAllById(Pageable pageable, List<String> productIds);
 }
